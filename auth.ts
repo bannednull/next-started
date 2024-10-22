2d89d8a061b186daa3bdfa1c48200bac106253a6
bannednull <bannednull@gmail.com>
2024-10-22 12:42:01 -0300

    next-auth providers credentials

--- auth.ts
+++ auth.ts
@@ -1,5 +1,32 @@
 import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+
+const credentialsSchema = z.object({
+  username: z.string().min(1, 'Username is required'),
+  password: z.string().min(1, 'Password is required'),
+});
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      credentials: {
+        username: { label: 'Username' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        try {
+          const { username, password } = credentialsSchema.parse(credentials);
+          console.log(username, password);
+          return { id: username, name: username };
+        } catch (error) {
+          console.log(error);
+          return null;
+        }
+      },
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
+  },
 });
